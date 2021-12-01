4bebfa1126e1e3b3e3a1e6b546c261d917c0c343
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-12-01 22:01:15 +0600

    addmin add event component created -static

--- src/pages/AdminAddEvent/AdminAddEvent.js
+++ src/pages/AdminAddEvent/AdminAddEvent.js
@@ -2,9 +2,27 @@ import React from 'react';
 
 const AdminAddEvent = () => {
     return (
-        <div>
-            <h3>This is admin add event component</h3>
-        </div>
+        <>
+            <section className="container my-5 p-5">
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input placeholder="Event Name" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleEventDescription" class="form-label">Event Description</label>
+                        <textarea class="form-control" placeholder="Event Description" id="exampleEventDescription" rows="5"></textarea>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="exampleImgUrl" class="form-label">Image URL</label>
+                        <input type="password" class="form-control" placeholder="Image URL" id="exampleImgUrl" />
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Add This Event</button>
+                </form>
+            </section>
+        </>
     );
 };
 
--- src/pages/AdminShowEvent/AdminShowEvent.js
+++ src/pages/AdminShowEvent/AdminShowEvent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AdminShowEvent = () => {
     return (
@@ -40,7 +41,10 @@ const AdminShowEvent = () => {
                         </tbody>
                     </table>
                 </section>
-                <button className="btn btn-primary">Add Event</button>
+                <Link to="/adminAddEvent">
+
+                    <button className="btn btn-primary">Add Event</button>
+                </Link>
             </section>
         </>
     );
