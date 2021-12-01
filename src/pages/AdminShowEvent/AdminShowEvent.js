fd2759d4f9144e3144ed4f176efcbc2fc9b531f5
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-12-01 21:49:54 +0600

    admin show event created - static

--- src/pages/AdminShowEvent/AdminShowEvent.js
+++ src/pages/AdminShowEvent/AdminShowEvent.js
@@ -2,9 +2,47 @@ import React from 'react';
 
 const AdminShowEvent = () => {
     return (
-        <div>
-            <h3>This is admin show event component</h3>
-        </div>
+        <>
+            <section className="container">
+
+
+                <div className="d-flex justify-content-center my-3">
+                    <h3>Volunteer register list</h3>
+                </div>
+
+                <section >
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email Id</th>
+                                <th scope="col">Registration Date</th>
+                                <th scope="col">Event Name</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                <td><button className="btn btn-danger">Delete</button></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                <td><button className="btn btn-danger">Delete</button></td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </section>
+                <button className="btn btn-primary">Add Event</button>
+            </section>
+        </>
     );
 };
 
